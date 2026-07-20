b25fad676ece958d63737c3e736f674ea1e828aa
redbirdfan <redbirdfan38@gmail.com>
2026-07-19 20:01:51 -0500

    conditional of hangman next to be completed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
+import { useState } from "react";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
+import words from "./wordList.json"
+
+const HEAD = (
+  <div style={{
+    width: "50px", height: "50px", borderRadius: "100%", border: "10px solid black", position: "absolute", top: "50px", right: "-30 px"}}
+/>
+)
+
 function App() {
-  return <h1>Hello!</h1>
+  const [wordToGuess, setWordToGuess] = useState( () => {
+        return words[Math.floor(Math.random()*words.length)]
+  })
+    
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+  
+  return 
+    <div 
+      style={{
+          maxWidth: "800px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "2rem",
+          margin: "0 auto",
+          alignItems: "center"
+          }}
+      >
+        <div style={{ fontSize: "2rem", textAlign: "center" }}>
+          Lose Win
+        </div>
+        <HangmanDrawing />
+        <HangmanWord />
+        <Keyboard />
+      </div>
 }
 
 export default App
