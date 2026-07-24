e50c36f757202fe8d0f612fbf58d5198ff259025
redbirdfan <redbirdfan38@gmail.com>
2026-07-23 21:03:23 -0500

    Letter now hidden/visible if guessed in array

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,12 +1,21 @@
 export function HangmanWord() {
+    const word = "test"
+    const guessedLetters = ["t"]
     return (
     <div style={{
         display:"flex",
         gap: ".25em",
         fontSize:"6rem",
-        textTransform:"uppercase",}}
+        textTransform:"uppercase",
+        }}
     >    
-        Test
+        {word.split("").map((letter, index) => (
+            <span 
+                style={{borderBottom: "4px solid black"}} key={index}>
+            <span style={{visibility: guessedLetters.includes(letter) ? "visible" : "hidden", }}>{letter}</span>
+        </span>
+    ))}
+
     </div>
     )
 }
