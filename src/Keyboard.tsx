dfb1ce719a1ddd0f4aa70ebad8de821539b6c8b1
redbirdfan <redbirdfan38@gmail.com>
2026-07-25 16:59:39 -0500

    alphabet now showing as 1 large column, need to alter to grid

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,3 +1,43 @@
+const key = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+]
 export function Keyboard(){
-    
+    return (
+        <div style={{display:"grid", 
+                    gridTemplateColumns:"repeat(auto-fix, minmax(75px, 1fr))", 
+                    gap:".5rem"
+                }}            
+        >
+            {keys.map(key=> {
+                return (
+                    <button key={key}>{key}</button>
+                )
+            })}
+        </div>
+    )
 }
